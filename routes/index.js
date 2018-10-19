2106e54eb515f2e4576ab606467affec786fa7ac
Otar <otar.adze@gmail.com>
2018-10-19 22:41:20 +0400

    add merge routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/merge', (req, res) => {
+	const image = req.body.image // base64
+	
+})
+
+
+
+
+
+
 module.exports = router;
