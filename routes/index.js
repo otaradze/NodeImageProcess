e8bbe64dd548c8fd901599cf36b9b267e2424f61
Otar <otar.adze@gmail.com>
2018-10-19 22:51:45 +0400

    show width

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+const { Image } = require('canvas')
+
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +10,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/merge', (req, res) => {
-	const image = req.body.image // base64
-	
+	const image = new Image();
+	image.src = req.body.image;
+	console.info('Image Size:', image.clientWidth);
+	res.send('OK');
 })
 
 
